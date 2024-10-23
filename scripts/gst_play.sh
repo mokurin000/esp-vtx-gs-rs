1347351298fe10b62ee3cb3cfd4b249061da249e
poly000 <1348292515@qq.com>
2024-10-23 16:03:43 +0800

    chore: remove unused files

--- scripts/gst_play.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 udpsrc port=5600  ! application/x-rtp, payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! autovideosink
--- scripts/set_netcard.sh
+++
@@ -1,9 +0,0 @@
-
-dev_name=$1
-
-nmcli device set $dev_name managed no
-ifconfig $dev_name down
-iw dev $dev_name set monitor otherbss
-ifconfig $dev_name up
-iw dev $dev_name set channel 11
-
